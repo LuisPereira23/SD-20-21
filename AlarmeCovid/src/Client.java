f1742f1a4f31feb29873131c93bf2c1c89279c47
haz145 <saralexandramarques@gmail.com>
2020-12-28 18:41:24 +0000

    menus

--- AlarmeCovid/src/Client.java
+++ AlarmeCovid/src/Client.java
@@ -10,139 +10,107 @@ public class Client {
     private final static int port = 34567;
     private final static String ip = "localhost";
     private static Socket socket;
-    public static String username = null;
-
     public static Scanner in = new Scanner(System.in);
-    public static DataOutputStream output = null;
-
 
     public static void main(String args[]) throws Exception{
         socket = new Socket(ip,port);
-        menu();
+        mainMenu();
     }
 
-    public static User parseLine (String userInput) {
-        String[] tokens = userInput.split(" ");
-
-        return new User(
-                tokens[0],
-                tokens[1]);
-    }
-
-    public static String initialMenu(){
-        System.out.println("Menu");
-        System.out.println("Application #1");
-        System.out.println("Exit #2");
-        String option = in.next();
-
-        if ( !(option.equals("1") || option.equals("2"))){
-            option = initialMenu();
-        }
-        return option;
-    }
-
-    public static void menu() throws IOException, ClassNotFoundException{
-        String option;
-        option = initialMenu();
-
-        do{
-            if(option.equals("1")){
-                authUser();
-            }else if(option.equals("2")){
-                System.exit(0);
-            }
-        }
-        while(true);
-    }
-
-    public static String userMenu(){
+    public static void mainMenu() throws IOException{
+        System.out.println("\n****** Main Menu ******\n");
         System.out.println("1 - Login");
-        System.out.println("2 - Register");
+        System.out.println("2 - Registar Utilizador");
+        System.out.println("0 - Sair");
         String option = in.next();
 
-        if ( !(option.equals("1") || option.equals("2"))){
-            option = userMenu();
-        }
-
-        return option;
-    }
-
-    public static void authUser() throws IOException{
-        String option;
-
-        option = userMenu();
-
-        if(option.equals("1")){
-            System.out.println("Login");
-            in.nextLine();
-            System.out.println("#Username:");
-            String user = in.nextLine();
-            System.out.println("password:");
-            String pass = in.nextLine();
-
-            DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-            out.writeInt(1);
-            out.flush();
-            String userInput = user + " " + pass;
-            User newUser = parseLine(userInput);
-            newUser.serialize(out);
-
-            DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-            Boolean auth = in.readBoolean();
-            System.out.println(auth);
-            if(auth){
-                username = user;
-                authMenu();
-            }
-            else{
-                System.out.println("Failed Authentication\n");
-            }
-        }else{
-
-            if(option.equals("2")){
-                System.out.println("Register");
-                in.nextLine();
-                System.out.println("Username");
-                String user = in.nextLine();
-                System.out.println("Password");
-                String pass = in.nextLine();
-
-                DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-
-                out.writeInt(2);
-                out.flush();
-                String userInput = user + " " + pass;
-                User newUser = parseLine(userInput);
-                newUser.serialize(out);
+        switch (option) {
+            case "1" -> userLogin();
+            case "2" -> userRegister();
+            case "0" -> System.exit(0);
+            default -> {
+                System.out.println("Invalid Option.\n");
+                mainMenu();
             }
         }
     }
 
-    public static void authMenu() {
-        System.out.println("Menu");
-        System.out.println("test");
+    public static void userLogin() throws IOException{
+        System.out.println("-- Login --");
+        in.nextLine();
+        System.out.println("Username:");
+        String user = in.nextLine();
+        System.out.println("Password:");
+        String pass = in.nextLine();
+
+        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        out.writeInt(1);
+        out.flush();
+        User newUser = new User(user, pass);
+        newUser.serialize(out);
+
+        DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        Boolean auth = in.readBoolean();
+        System.out.println(auth); //debug
+
+        if (auth) userMenu(newUser);
+        else{
+            System.out.println("Failed Authentication.\n");
+            mainMenu();
+        }
+    }
+
+    public static void userRegister() throws IOException{
+        System.out.println("-- Register --");
+        in.nextLine();
+        System.out.println("Username:");
+        String user = in.nextLine();
+        System.out.println("Password:");
+        String pass = in.nextLine();
+
+        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+        out.writeInt(2);
+        out.flush();
+        User newUser = new User(user, pass);
+        newUser.serialize(out);
+
+        System.out.println("Registered.\n");
+        mainMenu();
+    }
+
+    public static void userMenu(User user) throws IOException {
+        System.out.println("\n****** User Menu ******");
+        System.out.println("[ Bem-vindo " + user.getUsername() + " ]\n");
+        System.out.println("1 - Consultar dados de uma localização");
+        System.out.println("2 - Comunicar infeção");
+        //System.out.println("3 - etc...");
+        System.out.println("0 - Sair");
         String option = in.next();
 
-        do {
-            if(option.equals("2")){
-                //menu();
+        switch (option) {
+            case "1" -> localInfo();
+            case "2" -> reportCovid(user);
+            //case "3" -> ...
+            case "0" -> {
+                //logout(user);
+                mainMenu();
             }
-            if(option.equals("1")){
-                //  menu();
+            default -> {
+                System.out.println("Invalid Option.\n");
+                userMenu(user);
             }
-            if(option.equals("3")) {
-                // menu();
-            }
-            if(option.equals("4")){
-                // logout();
-            }
-            else{
-                if (!(option.equals("1") || option.equals("2") || option.equals("3")))
-                    System.out.println("Invalid Option");
-                authMenu();
-            }
-        } while(!(option.equals("1") || option.equals("2") || option.equals("3")));
+        }
     }
+
+
+    public static void localInfo(){
+
+    };
+
+    public static void reportCovid(User user){
+
+    };
 
 
 }
